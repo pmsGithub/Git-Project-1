c1606ce3841c51d031274bf78f554aa068b8bc47
PMS <paul@sweazey.com>
2020-07-03 14:11:15 +0200

    Initial Commit

---
+++ Git Project 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Project 1
+//
+//  Created by Paul M Sweazey on 2020.07.03.
+//  Copyright © 2020 Paul M Sweazey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
