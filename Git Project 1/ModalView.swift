7a2c05db940c1284560f5b2bbbeef00f539d8051
PMS <paul@sweazey.com>
2020-07-03 15:02:44 +0200

    Added Navigation View

--- Git Project 1/ModalView.swift
+++ Git Project 1/ModalView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ModalView: View {
     var body: some View {
-        Text("This is the modal View!")
+        VStack {
+            Text("This is the Second View!")
+        }.navigationBarTitle("Second View", displayMode: .inline)
     }
 }
 
